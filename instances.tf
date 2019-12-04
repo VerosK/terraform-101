219539dc45c27d9eca840a12285d439f3348dbf3
Veros Kaplan <VerosK@users.noreply.github.com>
2019-12-04 14:13:16 +0100

    Add random user-id to names

--- instances.tf
+++ instances.tf
@@ -2,25 +2,36 @@ data "local_file" "ssh_key" {
   filename = pathexpand("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_key_pair" "access_key" {
-  key_name = "workshop access key"
-  public_key = "${data.local_file.ssh_key.content}"
+resource "random_string" "user_id" {
+  length = 5
+  lower = true
+  upper = false
+  special = false
+  number = false
 }
 
+resource "aws_key_pair" "access_key" {
+  key_name = "workshop - ${random_string.user_id.result}"
+  public_key = data.local_file.ssh_key.content
+}
 
 resource "aws_instance" "webserver" {
   ami = "ami-dd3c0f36"
   instance_type = "t3.micro"
-  key_name = "${aws_key_pair.access_key.key_name}"
+  key_name = aws_key_pair.access_key.key_name
   monitoring = false
 
   associate_public_ip_address = true
 
   tags = {
-    Name = "Webserver API server"
+    Name = "Workshop - ${random_string.user_id.result}"
     Purpose = "Terraform workshop"
   }
+}
 
+resource "local_file" "ansible_inventory" {
+  filename = "hosts.txt"
+  content = "${aws_instance.webserver.public_dns}"
 }
 
 output "instance_ip" {
