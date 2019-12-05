87c21cf53179a190018348e82589066c14065f23
Veros Kaplan <VerosK@users.noreply.github.com>
2019-12-05 10:45:00 +0100

    Remove LB from eu-west

--- load-balancer.tf
+++ load-balancer.tf
@@ -29,23 +29,3 @@ resource "aws_elb" "central-lb" {
   }
 }
 
-
-resource "aws_elb" "west-lb" {
-  provider = "aws.aws-eu-west"
-
-  name               = "terraform-${random_string.user_id.result}"
-  availability_zones = ["eu-west-1a", "eu-west-1b"]
-
-  listener {
-    instance_port     = 8000
-    instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
-  }
-
-  tags = {
-    Name = "terraform--${random_string.user_id.result}"
-  }
-}
-
-
