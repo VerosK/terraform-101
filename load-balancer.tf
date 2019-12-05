68466225ad6191a17e0b5555b78fd99f0c8a88a3
Veros Kaplan <VerosK@users.noreply.github.com>
2019-12-05 13:38:39 +0100

    Add LB healthecks

--- load-balancer.tf
+++ load-balancer.tf
@@ -5,8 +5,18 @@ resource "aws_elb" "central-lb" {
   name               = "terraform-${random_string.user_id.result}"
   availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
 
+  security_groups = [ data.aws_security_group.default_group.id ]
+
+  health_check {
+    healthy_threshold = 2
+    interval = 60
+    target = "HTTP:80/"
+    timeout = 10
+    unhealthy_threshold = 10
+  }
+
   listener {
-    instance_port     = 8000
+    instance_port     = 80
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
