6bec6f2615099ff1c63f6d5a454053460b917d37
Věroš Kaplan <verosk@noreply.users.github.com>
2021-01-29 13:14:24 +0100

    add zones config

--- load-balancer.tf
+++ load-balancer.tf
@@ -1,25 +1,31 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 
 resource "aws_elb" "central-lb" {
   #provider = aws.aws-eu-central
 
-  name               = "terraform-${random_string.user_id.result}"
-  availability_zones = ["eu-central-1a", "eu-central-1b", "eu-central-1c"]
+  name = "terraform-${random_string.user_id.result}"
+  availability_zones = data.aws_availability_zones.available.names
 
-  security_groups = [data.aws_security_group.default_group.id]
+
+  security_groups = [
+    data.aws_security_group.default_group.id]
 
   health_check {
-    healthy_threshold   = 2
-    interval            = 60
-    target              = "HTTP:80/"
-    timeout             = 10
+    healthy_threshold = 2
+    interval = 60
+    target = "HTTP:80/"
+    timeout = 10
     unhealthy_threshold = 10
   }
 
   listener {
-    instance_port     = 80
+    instance_port = 80
     instance_protocol = "http"
-    lb_port           = 80
-    lb_protocol       = "http"
+    lb_port = 80
+    lb_protocol = "http"
   }
 
   tags = {
