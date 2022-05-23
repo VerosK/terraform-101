c5c0bd8396384cf2c39ce77098252b072b1c05d1
Věroš Kaplan <veros.kaplan@wftech.cz>
2022-05-23 22:52:24 +0200

    chore: database

---
+++ 02-rds-variables/00-providers.tf
@@ -0,0 +1,15 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
---
+++ 02-rds-variables/database.tf
@@ -0,0 +1,37 @@
+
+resource "aws_security_group" "mysql" {
+  name   = "mysql-access"
+  vpc_id = data.aws_vpc.main.id
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  name                    = "main"
+  identifier              = "tf-${random_string.suffix.result}"
+  engine                  = "mariadb"
+  engine_version          = "10.6.7"
+  backup_retention_period = 1
+
+  username            = var.db_user
+  password            = var.db_password
+  publicly_accessible = false
+  skip_final_snapshot = true
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage     = 5
+  max_allocated_storage = 15
+
+  vpc_security_group_ids = [aws_security_group.mysql.id]
+
+  // apply_immediately = true
+}
+
+output "database_host" {
+  value = aws_db_instance.mysql.address
+}
