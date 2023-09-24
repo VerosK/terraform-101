8452b4d0da8b1344643329039d22c5a887d24829
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-09-24 15:08:04 +0200

    feat: add Chuck norris joke to DNS record

--- 05-another-provider/record.tf
+++ 05-another-provider/record.tf
@@ -1,9 +1,20 @@
-
-
-resource "dns_a_record_set" "www" {
-  zone      = "dyn.gpx.cz."
-  name      = "www"
-  addresses = ["127.0.0.1"]
-  ttl       = 900
+data "http" "chuck_norris" {
+  url = "https://api.chucknorris.io/jokes/random"
 }
 
+locals {
+  joke = jsondecode(data.http.chuck_norris.body).value
+}
+
+resource "dns_txt_record_set" "magic" {
+  zone = "dyn.gpx.cz."
+  name = "chuck-norris"
+  txt = [
+     local.joke
+  ]
+  ttl = 60
+}
+
+output "a_joke" {
+  value = jsondecode(data.http.chuck_norris.body).value
+}
