7206e59073a48eb516e107a444a53497c900446f
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-09-26 16:36:23 +0200

    chore: aws provider add

--- 01-init/00-providers.tf
+++ 01-init/00-providers.tf
@@ -3,6 +3,10 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+    random = {
+      source = "hashicorp/random"
     }
   }
 }
