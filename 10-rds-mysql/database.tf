a163b730ac12a5cbfe08c994a41d17ccb22152b3
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-09-26 16:40:56 +0200

    chore: add outputs

---
+++ 10-rds-mysql/database.tf
@@ -0,0 +1,43 @@
+
+resource "aws_security_group" "mysql" {
+  name   = "mysql-access-${random_string.suffix.result}"
+  vpc_id = data.aws_vpc.main.id
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 0
+    protocol    = "icmp"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_db_instance" "mysql" {
+  db_name                 = "main"
+  identifier              = "tf-maria-${random_string.suffix.result}"
+  engine                  = "mariadb"
+  engine_version          = "10.6.13"
+  backup_retention_period = 1
+
+  username            = var.db_user
+  password            = var.db_password
+  publicly_accessible = false
+  skip_final_snapshot = true
+
+  instance_class = "db.t3.micro"
+
+  allocated_storage     = 5
+  max_allocated_storage = 15
+
+  vpc_security_group_ids = [aws_security_group.mysql.id]
+
+  // apply_immediately = true
+}
+
+output "database_host" {
+  value = aws_db_instance.mysql.address
+}
